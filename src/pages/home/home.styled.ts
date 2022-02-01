dddb12b7f58bf7da894a22827fa0e2ac188e5eb1
Anastasiia <anastsamot@gmail.com>
2022-02-01 09:01:08 +0200

    Add files via upload

--- src/pages/home/home.styled.ts
+++ src/pages/home/home.styled.ts
@@ -5,9 +5,54 @@ const StyledHome = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
+  flex-direction: column;
+
   justify-content: center;
   align-items: center;
   background-color: ${lightGreen85}; ;
 `;
 
-export { StyledHome };
+const StyledHomeTitle = styled.div`
+  width: 100%;
+  height: 10%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const StyledHomeContent = styled.div`
+  width: 100%;
+  height: 90%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledHabbitBlock = styled.div`
+  width: 100%;
+  // height: 90%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledHabbitTitle = styled.div`
+  width: 100%;
+  height: 90%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledHabbitTracker = styled.div`
+  width: 100%;
+  height: 90%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export {
+  StyledHome,
+  StyledHomeTitle,
+  StyledHomeContent,
+  StyledHabbitBlock,
+  StyledHabbitTitle,
+  StyledHabbitTracker,
+};
